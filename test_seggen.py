876b3b784fc9d3942a9bb45cde2a78e072e01493
sirin <sirin.neslihan@gmail.com>
2012-12-10 23:33:14 +0200

    Added alternative select best fitness individual function. this approach needs improvements.

--- test_seggen.py
+++ test_seggen.py
@@ -9,6 +9,10 @@ from test.utility import Utility
 test_list = [1,0,1]
 pareto_set = []
 
+#return a pair which has greatest key
+def choose_best(dict):
+    return sorted(dict.iteritems(),reverse=True)[0]
+
 class TestGene(BitGene):
 
     mutProb = 0.1
@@ -66,12 +70,15 @@ class TestOrganismPopulation(Population):
 
     initPopulation = 10
     species = TestOrganism
-
     # cull to this many children after each generation
     childCull = 10
 
     # number of children to create after each generation
     childCount = 40
+
+    def override_best(self,list):
+        list.sort()
+        return list[0]
 
 # start with a population of 10 random organisms
 ph = TestOrganismPopulation()
@@ -87,6 +94,11 @@ def main(nfittest=10, nkids=100):
 
         pop_size = len(individuals)
         pareto_set = utility.non_dominated(individuals)
+        pareto_set_official =[]
+        for x in pareto_set:
+            pareto_set_official.append(' '.join(str(elem) for elem in x))
+        #print pareto_set_official[0]
+
         pareto_hardness_list = []
         pareto_hardness_dict = {}
         population_strength_list = []
@@ -95,28 +107,29 @@ def main(nfittest=10, nkids=100):
         print "population: %s" % ph
         print "pareto set: %s" % pareto_set
 
-        for pind in pareto_set:
+        for pind, preal in zip(pareto_set, pareto_set_official):
             count = 0
             for ind in individuals:
                 if utility.dominates(pind, ind):
                     count += 1
             pareto = count / pop_size+1
             pareto_hardness_list.append(pareto)
-            pareto_hardness_dict[pareto] = pind
+            pareto_hardness_dict[pareto] = preal
 
-        #print "pareto hardness list: %s" % pareto_hardness_list
+        print "pareto hardness list: %s" % pareto_hardness_list
         print "pareto hardness dict: %s" % pareto_hardness_dict
 
-        for ind in individuals:
+        for ind, ireal in zip(individuals,ph):
             sum_fit = 1.0
-            for pind,fit in zip(pareto_set,pareto_hardness_list):
+            for pind, fit in zip(pareto_set,pareto_hardness_list):
                 if utility.dominates(pind, ind):
                     sum_fit += fit
             population_strength_list.append(1.0/sum_fit)
-            population_strength_dict[1.0/sum_fit] = ind
+            population_strength_dict[1.0/sum_fit] = ireal
 
-        #print "population fitness list: %s" % population_strength_list
+        print "population fitness list: %s" % population_strength_list
         print "population fitness dict: %s" % population_strength_dict
+        print choose_best(population_strength_dict)
 
         b = ph.best()
         print "Generation %s: %s Best=%s Average=%s)" % (
